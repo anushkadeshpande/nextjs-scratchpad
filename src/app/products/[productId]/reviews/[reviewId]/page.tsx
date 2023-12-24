eb8b43b072372011a3d1fc8a3586c7642869bc1a
anushkadeshpande <anushkaa5000@gmail.com>
2023-12-25 02:55:21 +0530

    feat: Added programmatic invocation of notFound page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,13 @@
+import { notFound } from "next/navigation";
+
 const Review = ({params}: {params: {
   productId: string;
   reviewId: string
 }}) => {
+
+  if(parseInt(params.reviewId) > 1000)
+    notFound()
+
   return <h1>Review {params.reviewId} for product {params.productId}</h1>
 }
 
