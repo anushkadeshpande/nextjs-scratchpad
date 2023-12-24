323dc2f91c586dea5141e7277648ab4f8ffcdaaa
anushkadeshpande <anushkaa5000@gmail.com>
2023-12-25 02:13:37 +0530

    feat: Added dynamic nested route

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,8 @@
+const Review = ({params}: {params: {
+  productId: string;
+  reviewId: string
+}}) => {
+  return <h1>Review {params.reviewId} for product {params.productId}</h1>
+}
+
+export default Review
