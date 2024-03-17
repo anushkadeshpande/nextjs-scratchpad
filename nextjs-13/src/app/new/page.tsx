5b87cd719f8e0cd4d07539c7f0191d56c5678359
anushkadeshpande <anushkaa5000@gmail.com>
2024-03-17 13:30:44 +0530

    feat: Added changes to pass userName to "/new" endpoint via queryParam and access it via "searchParams" prop

--- nextjs-13/src/app/new/page.tsx
+++ nextjs-13/src/app/new/page.tsx
@@ -3,34 +3,53 @@ import { redirect } from "next/navigation";
 import Link from "next/link";
 import React from "react";
 
-const sendMessage = async (data: FormData) => {
-  "use server"
-
-  // console.log("Hi") // logs in terminal
-
-  const message = data.get("message")?.valueOf()
-  if(typeof message !== "string" || message.length === 0) {
-    throw new Error("Invalid message")
+const sendMessage = async (user: string, data: FormData) => {
+  const message = data.get("message")?.valueOf();
+  if (typeof message !== "string" || message.length === 0) {
+    throw new Error("Invalid message");
   }
 
-  await prisma.messages.create({ data: { sender: "Anna", message: message, sentStatus: true } })
+  await prisma.messages.create({
+    data: { sender: user, message: message, sentStatus: true },
+  });
+};
 
-  redirect("/")
-}
+// searchParams lets you access query params sent to the page
 
-const New = () => {
+const New = ({ searchParams }: any) => {
   return (
     <>
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">New</h1>
       </header>
 
-      <form action={sendMessage} className="flex gap-2 flex-col">
-        <input type="text" name="message" className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"/>
+      <form
+        action={async (data) => {
+          "use server";
+          sendMessage(searchParams?.user, data);
+          redirect("/");
+        }}
+        className="flex gap-2 flex-col"
+      >
+        <input
+          type="text"
+          name="message"
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+        />
 
         <div className="flex gap-1 justify-end">
-          <Link href=".." className="border border-slate-300 text-slate-300 rounded px-2 py-1 outline-none focus-within:bg-slate-700">Cancel</Link>
-          <button type="submit" className="border border-slate-300 text-slate-300 rounded px-2 py-1 outline-none focus-within:bg-slate-700">Send</button>
+          <Link
+            href=".."
+            className="border border-slate-300 text-slate-300 rounded px-2 py-1 outline-none focus-within:bg-slate-700"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="border border-slate-300 text-slate-300 rounded px-2 py-1 outline-none focus-within:bg-slate-700"
+          >
+            Send
+          </button>
         </div>
       </form>
     </>
--- nextjs-13/src/app/page.tsx
+++ nextjs-13/src/app/page.tsx
@@ -14,7 +14,12 @@ export default async function Home() {
         <h1 className="text-2xl">Chat</h1>
         <Link
           className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-          href="/new"
+          href={{
+            pathname: '/new',
+            query: {
+              user: 'Anushka'
+            }
+          }}
         >
           New
         </Link>
